50053591ee6721aee49fa921f7fda00f5b59a0ba
Addinda Ayu Arsyah <addindaayuarsyah@gmail.com>
2025-08-07 09:37:53 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:prakteksqlite/pages/pelanggan_list.dart';
+
+void main() {
+  runApp(MaterialApp(title: 'Database Pelanggan', home: PelangganList()));
+}
